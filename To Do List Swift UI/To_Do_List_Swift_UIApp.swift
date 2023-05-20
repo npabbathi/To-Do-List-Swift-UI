30c8287fbc3be429af3037f33f6f9b254cfa2dc5
Nidhi Pabbathi <nidhi.pabbathi@utexas.edu>
2023-05-20 13:36:22 -0500

    Initial Commit

---
+++ To Do List Swift UI/To_Do_List_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_Swift_UIApp.swift
+//  To Do List Swift UI
+//
+//  Created by Nidhi Pabbathi on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
